696e372f1a9e585c8168a6d26854daa8794378ce
ilovelinuxporn <jake.crume@gmail.com>
2022-05-13 16:28:44 +1200

    Initial minimum functional commit

--- V5GL/Core.py
+++ V5GL/Core.py
@@ -1,1 +1,21 @@
 from vex import *
+
+class V5GLError(Exception):
+    def __init__(self, message="Sorry, V5GL encountered an internal error."):
+        self.message = message
+        super().__init__(self.message)
+
+class V5Application:
+    # bg and background are different names for the same thing, both will produce desired result. Refresh rate determines
+    # the screen refresh rate in hertz
+    def __init__(self, bg="white", background="", refresh_rate=60):
+        self.LoopSpeed = None
+        self.background = bg
+        if not background == "":
+            self.background = background
+        self.refreshRate = refresh_rate
+
+        brain.screen.clear_screen()
+        brain.screen.set_fill_color(self, self.background)
+        brain.screen.set_pen_color(self, Color.TRANSPARENT)
+        brain.screen.draw_rectangle(0, 0, 479, 239)
--- V5GL/Widgets.py
+++ V5GL/Widgets.py
@@ -1,1 +1,33 @@
 from vex import *
+
+class PushButton:
+    def __init__(self, x_coordinate, y_coordinate, width, height, bg="grey", background="", text="", drop_shadow=False,
+                 border_radius=0):
+        self.background = bg
+        if not background == "":
+            self.background = background
+        self.text = text
+        self.dropShadow = drop_shadow
+        self.xCoordinate = x_coordinate
+        self.yCoordinate = y_coordinate
+        self.width = width
+        self.height = height
+        self.borderRadius = border_radius
+        brain.screen.set_fill_color(self, self.background)
+        brain.screen.set_pen_color(self, Color.TRANSPARENT)
+
+        if not self.borderRadius == 0:
+            brain.screen.draw_rectangle(self.xCoordinate + self.borderRadius, self.yCoordinate + self.borderRadius, 
+                                        self.width - (self.borderRadius * 2), self.height - (self.borderRadius * 2))
+            brain.screen.draw_circle(self.xCoordinate - self.borderRadius, self.yCoordinate + self.borderRadius,
+                                     self.borderRadius)
+            brain.screen.draw_circle(self.xCoordinate - self.borderRadius + self.width,
+                                     self.yCoordinate + self.borderRadius, self.borderRadius)
+            brain.screen.draw_circle(self.xCoordinate - self.borderRadius + self.width,
+                                     self.yCoordinate - self.borderRadius + self.height, self.borderRadius)
+            brain.screen.draw_circle(self.xCoordinate - self.borderRadius,
+                                     self.yCoordinate + self.borderRadius + self.height, self.borderRadius)
+            brain.screen.draw_rectangle(self.xCoordinate + self.borderRadius, self.yCoordinate,
+                                        self.width - (self.borderRadius * 2), self.borderRadius)
+            brain.screen.draw_rectangle(self.xCoordinate + self.borderRadius, self.yCoordinate,
+                                        self.width - (self.borderRadius * 2), self.borderRadius)
